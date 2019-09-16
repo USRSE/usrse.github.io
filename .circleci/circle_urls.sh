57ae8bfa19e243764f9595bbca2a2d0d98fbdc82
Christina Maimone <christina.maimone@northwestern.edu>
2019-09-16 14:57:49 -0500

    changed logo file

--- .circleci/circle_urls.sh
+++ .circleci/circle_urls.sh
@@ -2,4 +2,4 @@ REPO_ID=$(curl https://api.github.com/repos/${CIRCLE_PROJECT_USERNAME}/${CIRCLE_
 echo "Repo ID is ${REPO_ID}"
 BASEURL=https://${CIRCLE_BUILD_NUM}-${REPO_ID}-gh.circle-artifacts.com/0/usrse.github.io
 sed -i "8 s,.*,baseurl: $BASEURL,g" "_config.yml"
-sed -i "9 s,.*,title-img: $BASEURL/assets/img/rse_logo_background.svg,g" "_config.yml"
+sed -i "9 s,.*,title-img: $BASEURL/assets/img/logo_600px.png,g" "_config.yml"
