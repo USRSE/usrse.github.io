1e6a117f39a4270c32eb815f79ae5fe5e0357cd6
vsoch <vsoch@users.noreply.github.com>
2022-03-31 16:59:25 -0600

    remove circle, add jobs tests to a main testing workflow
    Signed-off-by: vsoch <vsoch@users.noreply.github.com>

--- .circleci/circle_urls.sh
+++
@@ -1,7 +0,0 @@
-echo "CIRCLE_WORKFLOW_WORKSPACE_ID ${CIRCLE_WORKFLOW_WORKSPACE_ID}"
-echo "CIRCLE_BUILD_NUM ${CIRCLE_BUILD_NUM}"
-echo "CIRCLE_WORKFLOW_ID ${CIRCLE_WORKFLOW_ID}"
-echo "CIRCLE_BUILD_NUM ${CIRCLE_BUILD_NUM}"
-echo "CIRCLE_OIDC_TOKEN ${CIRCLE_OIDC_TOKEN}"
-BASEURL=https://output.circle-artifacts.com/output/job/${CIRCLE_JOB}/artifacts/${CIRCLE_BUILD_NUM}/usrse.github.io/index.html
-sed -i "8 s,.*,baseurl: $BASEURL,g" "_config.yml"
