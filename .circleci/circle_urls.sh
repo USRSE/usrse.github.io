03ba5babb6a35e167ac474ec24eeaa76cce6f51a
Luke R <luke.rasmussen@gmail.com>
2023-06-27 20:20:34 -0500

    Adjust CircleCI script to adjust url parameter since it uses baseurl

--- .circleci/circle_urls.sh
+++ .circleci/circle_urls.sh
@@ -1,2 +1,3 @@
 BASEURL=https://output.circle-artifacts.com/output/job/${CIRCLE_WORKFLOW_JOB_ID}/artifacts/${CIRCLE_NODE_INDEX}/usrse.github.io
 sed -i "8 s,.*,baseurl: $BASEURL,g" "_config.yml"
+sed -i "7 s,.*,url: "",g" "_config.yml"
