89b64f71ab7f9e26fe2445a072a962933d193ee7
Aaron Holmes <aaron@aaronholmes.net>
2023-06-28 13:43:35 -0700

    Fix issue with social share buttons.
    `site.url` does not contain the domain of the site, which breaks social buttons.
    This attempts to resolve what looks like a misconfiguration of Jekyll to correctly generate the URLs.
    
    Closes #1201
    
    Remove unused script.
    
    Closes #1201
    
    Fix typo in domain.
    
    Closes #1201

--- .circleci/circle_urls.sh
+++
@@ -1,3 +0,0 @@
-BASEURL=https://output.circle-artifacts.com/output/job/${CIRCLE_WORKFLOW_JOB_ID}/artifacts/${CIRCLE_NODE_INDEX}/usrse.github.io
-sed -i "8 s,.*,baseurl: $BASEURL,g" "_config.yml"
-sed -i "8 s,.*,url: https://usrse.org,g" "_config.yml"
